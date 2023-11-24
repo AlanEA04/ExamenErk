1ff51496e0b13dadc639ad262592bec03688a127
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-11-24 08:58:29 -0700

    RRRR

---
+++ Nudo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenErk
+{
+    public class Nudo
+    {
+
+        private string nombre ;
+        private int edad;
+        private Nudo siguiente;
+
+
+
+        public string Nombre
+        {
+            get{ return nombre; }
+            set{ nombre = value; }
+        }
+        public int Edad
+        {
+            get { return edad; } 
+            set { edad = value; }
+        }
+        public Nudo Siguiente 
+        {
+            get { return siguiente;}
+            set { siguiente = value; }
+        
+        }
+
+
+    }
+
+    //public string Nombre()
+    //{
+    //    get{ return nombre; }
+    //    set{ }
+    //}
+}
---
+++ Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenErk
+{
+    public class Persona
+    {
+        public Nudo Primero = null;
+        public Nudo Ultimo = null;
+
+
+
+    }
+
+
+    public void InsertarNombre()
+    {
+       Nudo Nuevo = new Nudo();
+        Console.Write("Ingresa un Nombre");
+        string Nombre = Console.ReadLine();
+        Nuevo.Nombre string (Console.ReadLine());
+         
+        /*Nudo Dos =new Nudo();
+        Console.WriteLine("ingresa la edad");
+        Dos.Edad = int.Parse(Console.ReadLine());
+        
+        if(Dos.Edad! == null)
+        {
+          
+
+
+        }*/
+
+    }
+
+
+
+
+
+}
